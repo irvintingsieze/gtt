d49851258fb55f1655a2174063d9b20308c91b50
irvintingsieze <irvintingg@gmail.com>
2021-07-20 23:48:50 +0800

    Update client-entity.entity.ts

--- be/gtt/src/client-entity/entities/client-entity.entity.ts
+++ be/gtt/src/client-entity/entities/client-entity.entity.ts
@@ -1,1 +1,13 @@
-export class ClientEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('client_entity')
+export class ClientEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  clientId: string;
+
+  @Column()
+  entityId: string;
+}
