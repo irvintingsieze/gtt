95c0807cf3e38024d2bba03b8105b4b5dd4ed155
irvintingsieze <irvintingg@gmail.com>
2021-07-20 23:03:49 +0800

    data

--- be/gtt/src/app.controller.ts
+++ be/gtt/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/json')
+  getJSON(): string {
+    return this.appService.getJSON();
+  }
 }
