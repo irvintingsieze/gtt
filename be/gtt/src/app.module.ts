8821f86ad5b60c280b6ba06176fd6471b78e8dc4
irvintingsieze <irvintingg@gmail.com>
2021-07-21 22:58:33 +0800

    client

--- be/gtt/src/app.module.ts
+++ be/gtt/src/app.module.ts
@@ -5,12 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientStatusModule } from './client-status/client-status.module';
 import { GttCheckModule } from './gtt-check/gtt-check.module';
 import { TradeModule } from './trade/trade.module';
+import { ClientStatus } from './client-status/entities/client-status.entity';
+import { ClientModule } from './client/client.module';
 @Module({
   imports: [
+    TypeOrmModule.forFeature([ClientStatus]),
     TypeOrmModule.forRoot(),
     ClientStatusModule,
     GttCheckModule,
     TradeModule,
+    ClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- be/gtt/src/app.service.ts
+++ be/gtt/src/app.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import * as apidata from './../data/gtt_api_data_clean.json';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ClientStatus } from './client-status/entities/client-status.entity';
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(ClientStatus)
+    private clientStatusRepository: Repository<ClientStatus>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
 
   getJSON() {
+    console.log(apidata.data[0]);
     return JSON.stringify(apidata);
   }
 }
